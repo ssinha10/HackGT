ca105a77fddc5e0fd56555c5c2e668dfdc3cc0b8
Varun Munjeti <vrunjeti@gmail.com>
2015-09-25 22:30:58 -0500

    set up boilerplate code

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name hackgtApp
+ * @description
+ * # hackgtApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('hackgtApp', [
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hackgtApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hackgtApp
+ */
+angular.module('hackgtApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
