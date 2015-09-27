fe0d1b9d74511560dd385739f9fa5d34404951f6
Qaceless <arjun.s.garg@gmail.com>
2015-09-26 22:07:31 -0400

    changed layout

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -26,6 +26,7 @@ module.exports = function(config) {
       'bower_components/angular/angular.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/Materialize/bin/materialize.js',
+      'bower_components/oauth-signature/dist/oauth-signature.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
